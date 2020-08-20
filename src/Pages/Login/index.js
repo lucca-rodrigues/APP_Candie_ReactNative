fd46bc1407953950f5bf5f8fb12e962951cdbf1b
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-08-20 16:43:33 -0300

    input login

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import {View, Text, TextInput, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 
 export default function Login({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function login(){
+    alert(`Login Realizado com Sucesso! Email: ${email} Password: ${password}`);
+  }
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={styles.logo}>Candie</Text>
       <Text>Faça o seu login</Text>
-      <TextInput placeholder="E-mail"/>
-      <TextInput placeholder="Senha"/>
-      <Button title="Entrar" onPress={() => navigation.navigate('Home')}/>
-      <Text onPress={() => navigation.navigate('Home')}>Não tenho uma conta?</Text>
-    </View> 
+      <TextInput 
+        placeholder="E-mail"
+        onChangeText={email => setEmail(email)}
+        defaultValue={email}
+        />
+      <TextInput 
+        placeholder="Senha"
+        onChangeText={password => setPassword(password)}
+        defaultValue={password}
+      />
+      <Button title="Entrar" onPress={login}/>
+      <Text
+        style={styles.createAccount} 
+        onPress={() => navigation.navigate('Home')}>
+          Não tenho uma conta?
+      </Text>
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  logo:{
+    color: '#000',
+    fontSize: 30,
+  },
+  createAccount: {
+    marginTop: 20
+  }
+});
